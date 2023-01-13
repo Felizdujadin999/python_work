bd5f2e3fb3c15a6e53449cf57b921beae70ff99d
Felizdujadin <ezeikefelix51@gmail.com>
2023-01-13 10:02:35 +0100

    updated

--- python_package/diamond_triangles.py
+++ python_package/diamond_triangles.py
@@ -1,32 +1,39 @@
-# def my_function2():
-#     for i in range(1, 21, 2):
-#         asterik = "*" * i
-#         print(f"{asterik:<2}")
-#
-#
-# print(my_function2())
-# print()
+def my_function2():
+    for i in range(1, 21, 2):
+        asterik = "*" * i
+        print(f"{asterik:<2}")
+    for i in range(21, 0, -2):
+        asterik = "*" * i
+        print(f"{asterik:2}")
 
 
-def my_function(number: int):
-    for i in range(1, number + 1):
+print(my_function2())
+print()
+
+asteriks = int(input("Enter number of asteriks: "))
+
+
+def my_function(input_):
+    for i in range(1, input_ + 1):
         asterik = " *" * i
         print(asterik, end="\n")
 
 
-print(my_function(10))
+my_function(asteriks)
+
+square = int(input("Enter number of asteriks:"))
 
 
-def my_square(number: int):
-    for count in range(1, number + 1):
-        if count == 1 or count == number:
-            print("* " * number)
+def my_square(input):
+    for count in range(1, input + 1):
+        if count == 1 or count == input:
+            print("* " * input)
         else:
-            in_between = " " * ((number * 2) - 3)
-            print("*{a}*".format(a=in_between))
+            in_between = " " * ((input * 2) - 3)
+            print("*{b}*".format(b=in_between))
 
 
-print(my_square(20))
+my_square(square)
 # for i in range(1, 21, 2):
 #     asterik = "*" * i
 #     print(f"{asterik:<2}")
